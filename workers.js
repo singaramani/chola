eb562754057d27e3ee2b65a0e145f7e9cb858f17
singaramani <singaramani@gmail.com>
2018-12-12 17:54:35 +0400

    Update workers.js

--- workers.js
+++ workers.js
@@ -5,10 +5,13 @@
 		fs = require('fs');
 		let client = httpc;
 		if (url.toString().indexOf("https") === 0) {client = httpsc;}
+		var data="";
+		console.log(url);
 		client.get(url, (resp) => {
 			resp.on('data', (chunk) => { data += chunk; });
 			resp.on('end', () => {
 				fs.writeFile("/data/token.txt", data.trim(), function(err) {
+				//fs.writeFile("D:\\token.txt", data.trim(), function(err) {
 					if(err) {return console.log("WriteFile Err::"+err);}
 					logme("WriteFile Done:: "+new Date().toLocaleString("en-US", {timeZone: "Asia/Kolkata"})+" IST");
 				});
@@ -17,4 +20,12 @@
 			logme(err);
 		});
     }
+	function getISTTime(){
+	  var istTimeStr = new Date().toLocaleString("en-US", {timeZone: "Asia/Kolkata"});
+	  var tmStr = istTimeStr.split(", ")[1];
+	  return tmStr;
+	}
+	 function logme(msg){
+	  console.log(getISTTime()+"| "+msg);
+	}
 }());
