27b1c97f462fd7b777264b62832c7f76e97a9498
singaramani <singaramani@gmail.com>
2018-12-12 19:24:28 +0400

    Update workers.js

--- workers.js
+++ workers.js
@@ -1,8 +1,28 @@
 (function() {
+	fs = require('fs');
 	module.exports.fetchWriteToken = function(url) {
 		const httpc = require('http'), 
-		httpsc     = require('https'),
-		fs = require('fs');
+		httpsc     = require('https');
+		let client = httpc;
+		if (url.toString().indexOf("https") === 0) {client = httpsc;}
+		var data="";
+		logme("Connecting..");
+		client.get(url, (resp) => {
+			resp.on('data', (chunk) => { data += chunk; });
+			resp.on('end', () => {
+				fs.writeFile("/data/token.txt", data.trim(), function(err) {
+				//fs.writeFile("D:\\token.txt", data.trim(), function(err) {
+					if(err) {return console.log("Token->Store.. Err::"+err);}
+					logme("Token->Store.. Done");
+				});
+			});
+		}).on("error", (err) => {
+			logme(err);
+		});
+    }
+	module.exports.wakeupServer = function(url) {
+		const httpc = require('http'), 
+		httpsc     = require('https');
 		let client = httpc;
 		if (url.toString().indexOf("https") === 0) {client = httpsc;}
 		var data="";
@@ -10,16 +30,20 @@
 		client.get(url, (resp) => {
 			resp.on('data', (chunk) => { data += chunk; });
 			resp.on('end', () => {
-				fs.writeFile("/data/token.txt", data.trim(), function(err) {
-				//fs.writeFile("D:\\token.txt", data.trim(), function(err) {
-					if(err) {return console.log("WriteFile Err::"+err);}
-					logme("WriteFile Done:: "+new Date().toLocaleString("en-US", {timeZone: "Asia/Kolkata"})+" IST");
-				});
+				logme("Wakeup sent..");
 			});
 		}).on("error", (err) => {
 			logme(err);
 		});
     }
+	function readToken() {
+		fs.readFile("/data/token.txt", "utf8", function(err, data){
+		//fs.readFile("D:\\token.txt", "utf8", function(err, data){
+			if(err) { return "file read error"; logme("file read error"); }
+			return data;
+		});
+    }
+	
 	function getISTTime(){
 	  var istTimeStr = new Date().toLocaleString("en-US", {timeZone: "Asia/Kolkata"});
 	  var tmStr = istTimeStr.split(", ")[1];
@@ -28,4 +52,7 @@
 	 function logme(msg){
 	  console.log(getISTTime()+"| "+msg);
 	}
+	
+	module.exports.readToken = readToken;
+	module.exports.logme = logme;
 }());
