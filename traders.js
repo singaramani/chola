fd2449d72573c389ca2e34d441e1d7b3f6944c3d
singaramani <singaramani@gmail.com>
2018-12-16 12:12:30 +0400

    Update traders.js

--- traders.js
+++ traders.js
@@ -1,36 +1,42 @@
-(function() {
+(function () {
 	const Upstox = require("upstox");
 	const fs = require('fs');
 	const Appconst = require("./appconstants");
 	const Worker = require("./workers");
-	
+
 	var appconts = Appconst.getAppConstants();
 	var upstox = new Upstox(appconts.appKey, appconts.appSecret);
 	upstox.setApiVersion(upstox.Constants.VERSIONS.Version_1_5_6);
-	
+
 	var n50 = Appconst.getN50();
 	var nfo = Appconst.getAllNFO();
-	
+
 	var qty = process.env.TQTY;
 	var trigger_offset = 0.15;
 	var price_offset = 0.10;
-	
-	function initSetToken(){
-		fs.readFile("/data/token.txt", "utf8", function(err, data){
-		//fs.readFile("D:\\token.txt", "utf8", function(err, data){
-			if(err) { return "file read error"; logme("file read error in traders.js"); } 
-			if(isJsonString(data)){
-			var codeData = JSON.parse(data);
-			upstox.setToken(codeData.token);
-			invokeSocket();
-			Worker.logme("Token set.. --"+codeData.token);
-			Worker.logme("Socket invoked..");}else{Worker.logme("token file parse error");}
+
+	function initSetToken() {
+		fs.readFile("/data/token.txt", "utf8", function (err, data) {
+			//fs.readFile("D:\\token.txt", "utf8", function(err, data){
+			if (err) {
+				return "file read error";
+				logme("file read error in traders.js");
+			}
+			if (isJsonString(data)) {
+				var codeData = JSON.parse(data);
+				upstox.setToken(codeData.token);
+				invokeSocket();
+				Worker.logme("Token set.. --" + codeData.token);
+				Worker.logme("Socket invoked..");
+			} else {
+				Worker.logme("token file parse error");
+			}
 		});
 
 	}
-	
+
 	function selectScrips_HL(sList, n, sType) {
-		Worker.logme("Getting "+sType+" scrips pclose & open");
+		Worker.logme("Getting " + sType + " scrips pclose & open");
 		var _promiseArray = [];
 		sList.forEach(function (scrip) {
 			var _scripPromise = new Promise(function (resolve, reject) {
@@ -50,7 +56,7 @@
 						});
 					})
 					.catch(function (error) {
-						Worker.logme("Err::" + scrip.sym + "::"+ error.message);
+						Worker.logme("Err::" + scrip.sym + "::" + error.message);
 						//reject(error);
 					});
 				});
@@ -77,7 +83,7 @@
 	function getHighLow(sList, tf) {
 		Worker.logme("Identifying first " + tf + "min HL");
 		var _promiseArray = [];
-		var ftf = (tf / 5)+1;
+		var ftf = (tf / 5) + 1;
 		sList.forEach(function (scrip) {
 			var _scripPromise = new Promise(function (resolve, reject) {
 					upstox.getOHLC({
@@ -123,110 +129,116 @@
 			placeOrderConditional(respArr);
 		});
 	}
-	
+
 	function placeOrderConditional(scripArr) {
-	  invokeSocket();
+		invokeSocket();
 		//var qty = 0;
-	  Worker.logme(" ");
+		Worker.logme(" ");
 		Worker.logme("Order plan:");
-	  Worker.logme("--------------------------------------------------");
-	  var orderPrepArray = [];
+		Worker.logme("--------------------------------------------------");
+		var orderPrepArray = [];
 		scripArr.forEach(function (scrip) {
-		Worker.logme(scrip.sym + "|buy above " + scrip.high + "|sell below " + scrip.low + "|qty:" + qty);
-		orderPrepArray.push({
-		  txnDesc:"Sell", 
-		  txnType:"s", 
-		  ex:scrip.ex, 
-		  sym:scrip.sym,
-		  p:scrip.low - trigger_offset,
-		  tp:scrip.low - price_offset,
-		  q:qty
+			Worker.logme(scrip.sym + "|buy above " + scrip.high + "|sell below " + scrip.low + "|qty:" + qty);
+			orderPrepArray.push({
+				txnDesc: "Sell",
+				txnType: "s",
+				ex: scrip.ex,
+				sym: scrip.sym,
+				p: scrip.low - trigger_offset,
+				tp: scrip.low - price_offset,
+				q: qty
+			});
+			orderPrepArray.push({
+				txnDesc: "Buy",
+				txnType: "b",
+				ex: scrip.ex,
+				sym: scrip.sym,
+				p: scrip.high + trigger_offset,
+				tp: scrip.high + price_offset,
+				q: qty
+			});
 		});
-		orderPrepArray.push({
-		  txnDesc:"Buy", 
-		  txnType:"b", 
-		  ex:scrip.ex, 
-		  sym:scrip.sym,
-		  p:scrip.high + trigger_offset,
-		  tp:scrip.high + price_offset,
-		  q:qty
-		});
-		});
-	  Worker.logme("--------------------------------------------------");
-	  placeOrdr(orderPrepArray);
+		Worker.logme("--------------------------------------------------");
+		placeOrdr(orderPrepArray);
 	}
 
 	function placeOrdr(orderPrepArray) {
-	  Worker.logme(" ");
-	  Worker.logme("Placing orders..");
-	  orderPrepArray.forEach(function (ordr) {
-		// Worker.logme(JSON.stringify({
-		//   "transaction_type":ordr.txnType,
-		//   "exchange":ordr.ex,
-		//   "symbol": ordr.sym,
-		//   "quantity": ordr.q,
-		//   "order_type":"sl",
-		//   "product": "I",
-		//   "price":ordr.p.toFixed(2),
-		//   "trigger_price":ordr.tp.toFixed(2)
-		// }));
-		upstox.placeOrder({
-			"transaction_type": ordr.txnType,
-			"exchange": ordr.ex,
-			"symbol": ordr.sym,
-			"quantity": ordr.q,
-			"order_type": "sl",
-			"product": "I",
-			"price": ordr.p.toFixed(2),
-			"trigger_price": ordr.tp.toFixed(2)
-		})
-		.then(function (response) {
-			Worker.logme(response.data.order_id + "|" + sym + "|" + response.data.status);
-		})
-		.catch(function (error) {
-			//done(error);
-			//Worker.logme("PlaceOrder::"+JSON.stringify(error));
-			//Worker.logme(ordr.sym+"|"+error.message+"|"+error.error.reason)
+		Worker.logme(" ");
+		Worker.logme("Placing orders..");
+		orderPrepArray.forEach(function (ordr) {
+			// Worker.logme(JSON.stringify({
+			//   "transaction_type":ordr.txnType,
+			//   "exchange":ordr.ex,
+			//   "symbol": ordr.sym,
+			//   "quantity": ordr.q,
+			//   "order_type":"sl",
+			//   "product": "I",
+			//   "price":ordr.p.toFixed(2),
+			//   "trigger_price":ordr.tp.toFixed(2)
+			// }));
+			upstox.placeOrder({
+				"transaction_type": ordr.txnType,
+				"exchange": ordr.ex,
+				"symbol": ordr.sym,
+				"quantity": ordr.q,
+				"order_type": "sl",
+				"product": "I",
+				"price": ordr.p.toFixed(2),
+				"trigger_price": ordr.tp.toFixed(2)
+			})
+			.then(function (response) {
+				Worker.logme(response.data.order_id + "|" + sym + "|" + response.data.status);
+			})
+			.catch(function (error) {
+				//done(error);
+				//Worker.logme("PlaceOrder::"+JSON.stringify(error));
+				//Worker.logme(ordr.sym+"|"+error.message+"|"+error.error.reason)
+			});
 		});
-	  });
 	}
 
-	function invokeSocket(){
-	  upstox.connectSocket()
-	  .then(function() {
-		Worker.logme("Socket connected.");
-		upstox.on("orderUpdate", function(message) {
-		  if(message.status=="trigger pending"){
-			Worker.logme(message.order_id+" - Trgr. pending : "+message.symbol+" "+message.quantity+"@"+message.trigger_price);
-		  }
-		  if(message.status=="complete"){
-			Worker.logme(message.order_id+" - Completed : "+message.symbol+" "+message.traded_quantity+"@"+message.average_price);
-			placeTargerOrder(message);
-		  }		
-		  if(message.status=="cancelled"){
-			Worker.logme(message.order_id+" - Cancelled : "+message.symbol+" "+message.traded_quantity+"@"+message.average_price);
-		  }	    
+	function invokeSocket() {
+		upstox.connectSocket()
+		.then(function () {
+			Worker.logme("Socket connected.");
+			upstox.on("orderUpdate", function (message) {
+				if (message.status == "open") {
+					Worker.logme(message.order_id + " - Open : " + message.symbol + " " + message.quantity + " @ " + message.trigger_price);
+				}				
+				if (message.status == "trigger pending") {
+					Worker.logme(message.order_id + " - Trgr. pending : " + message.symbol + " " + message.quantity + " @ " + message.trigger_price);
+				}
+				if (message.status == "complete") {
+					Worker.logme(message.order_id + " - Completed : " + message.symbol + " " + message.traded_quantity + " @ " + message.average_price);
+					placeTargerOrder(message);
+				}
+				if (message.status == "cancelled") {
+					Worker.logme(message.order_id + " - Cancelled : " + message.symbol + " " + message.traded_quantity + " @ " + message.average_price);
+				}
+				if (message.status == "rejected") {
+					Worker.logme(message.order_id + " - Rejected : " + message.symbol + " " + message.traded_quantity + " @ " + message.average_price);
+				}				
+			});
+			upstox.on("positionUpdate", function (message) {
+				Worker.logme("Position Updated. " + JSON.stringify(message));
+			});
+			upstox.on("tradeUpdate", function (message) {
+				//Worker.logme("Trade Updated. " + JSON.stringify(message));
+			});
+			upstox.on("liveFeed", function (message) {
+				//Worker.logme("Live Feed. - " + JSON.stringify(message));
+			});
+			upstox.on("disconnected", function (message) {
+				Worker.logme("Socket disconnected.");
+			});
+			upstox.on("error", function (error) {
+				Worker.logme("Socket on_error:" + JSON.stringify(error));
+			});
+		}).catch(function (err) {
+			//Worker.logme("Socket error:" + JSON.stringify(err));
 		});
-		upstox.on("positionUpdate", function(message) {
-		  Worker.logme("Position Updated. "+JSON.stringify(message));
-		});
-		upstox.on("tradeUpdate", function(message) {
-		  //Worker.logme("Trade Updated. " + JSON.stringify(message));
-		});
-		upstox.on("liveFeed", function(message) {
-		  //Worker.logme("Live Feed. - " + JSON.stringify(message));
-		});
-		upstox.on("disconnected", function(message) {
-		  Worker.logme("Socket disconnected.");
-		});
-		upstox.on("error", function(error) {
-		  Worker.logme("Socket on_error:" + JSON.stringify(error));
-		});
-	  }).catch(function(err) {
-		//Worker.logme("Socket error:" + JSON.stringify(err));
-	  });
 	}
-	
+
 	Array.prototype.sortBy = function (p) {
 		return this.slice(0).sort(function (a, b) {
 			return (a[p] > b[p]) ? 1 : (a[p] < b[p]) ? -1 : 0;
@@ -234,112 +246,117 @@
 	}
 	function isJsonString(str) {
 		try {
-		  var json = JSON.parse(str);
-		  return (typeof json === 'object');
+			var json = JSON.parse(str);
+			return (typeof json === 'object');
 		} catch (e) {
-		  return false;
+			return false;
 		}
 	}
-	
-	function placeTargerOrder(pos){
-	 Worker.logme("Placing target for "+pos.symbol+" "+pos.traded_quantity+" @ "+"[1.5%]");
-	 upstox.placeOrder({
-			"transaction_type": (pos.transaction_type=="B")?"s":"b",
+
+	function placeTargerOrder(pos) {
+		var txtTyp = (pos.transaction_type == "B" || pos.transaction_type == "b") ? "s" : "b";
+		var calc_tp = (txtTyp == "b") ? 1-(appconts.targetpcent/100) : 1+(appconts.targetpcent/100);
+		var tp = (Math.round((pos.average_price) * calc_tp * 20) / 20);
+		Worker.logme("Placing target order for " + pos.symbol + " " + txtTyp + " " + pos.traded_quantity + " @ " + tp + " ["+appconts.targetpcent+"%]");
+		upstox.placeOrder({
+			"transaction_type": txtTyp,
 			"exchange": pos.exchange,
 			"symbol": pos.symbol,
 			"quantity": pos.traded_quantity,
 			"order_type": "l",
 			"product": "I",
-			"price":(Math.round((pos.average_price)*1.015*20)/20)
+			"price": tp
 		})
 		.then(function (response) {
-			Worker.logme(""+response.data.order_id + "|" + pos.symbol + "|" + response.data.status);
+			Worker.logme("" + response.data.order_id + "|" + pos.symbol + "|" + response.data.status);
 		})
 		.catch(function (error) {
 			//done(error);
-			Worker.logme(JSON.stringify(error));
+			Worker.logme("Placing target order error. "+JSON.stringify(error));
 		});
-	}
-	
-	function cancellAllOrders(){
-	  upstox.cancelAllOrder({})
-	  .then(function (response){
-		  Worker.logme(response.message);
-		  Worker.logme("Orders:"+response.data);
-	  }).catch(function(error){
-	   //done(error);
-	   Worker.logme("Error cancelling all orders at once");
-	  });
-	}
-	
-	function getCurrentPos(){
-		upstox.getPositions()
-		.then(function (response){
-		 var tPos = response.data;
-		 var mtm = 0;
-		 Worker.logme("----------------------");
-		 tPos.forEach(function (pos) {
-		  var pnl = 0;
-		  if(pos.realized_profit && !isNaN(pos.realized_profit)){
-		   pnl = pnl + Number(pos.realized_profit);
-		  }
-		  if(pos.unrealized_profit && !isNaN(pos.unrealized_profit)){
-		   pnl = pnl + Number(pos.unrealized_profit);
-		   Worker.logme(pos.symbol + "|" + pos.net_quantity + "|" + pnl);
-		   mtm = mtm + pnl;
-		  }
-		 });
-		 Worker.logme("----------------------");
-		 Worker.logme("MTM:"+mtm);
-		 Worker.logme("----------------------");
-		}).catch(function(error){
-		 //done(error);
-		 Worker.logme("Error getting positions");
-		});
-	}
-	
-	function exitAllPos(){
-	 upstox.getPositions()
-		.then(function (response){
-		 var tPos = response.data;
-		 Worker.logme("Exiting all open positions..");
-		 tPos.forEach(function (pos) {
-		  if(pos.net_quantity!=0){
-		   upstox.placeOrder({
-			"transaction_type": (pos.net_quantity>0)?"s":"b",
-			"exchange": pos.exchange,
-			"symbol": pos.symbol,
-			"quantity": pos.net_quantity,
-			"order_type": "m",
-			"product": "I"
-		})
-		.then(function (response) {
-			Worker.logme(response.data.order_id + "|" + pos.symbol + "|" + response.data.status);
-		})
-		.catch(function (error) {
-			//done(error);
-			Worker.logme(JSON.stringify(error));
-		});
-		  }
-		 });
-		}).catch(function(error){
-		 //done(error);
-		 Worker.logme("Error getting positions");
-		});
-	}
-	
-	function diconnectSock(){
-	 upstox.closeSocket();
 	}
 
-	
+	function cancellAllOrders() {
+		upstox.cancelAllOrder({})
+		.then(function (response) {
+			Worker.logme(response.message);
+			Worker.logme("Orders:" + response.data);
+		}).catch(function (error) {
+			//done(error);
+			Worker.logme("Error cancelling all orders at once. "+JSON.stringify(error));
+		});
+	}
+
+	function getCurrentPos() {
+		upstox.getPositions()
+		.then(function (response) {
+			var tPos = response.data;
+			var mtm = 0;
+			Worker.logme("MTM--------------------------------------------");
+			tPos.forEach(function (pos) {
+				var pnl = 0;
+				if (pos.realized_profit && !isNaN(pos.realized_profit)) {
+					pnl = pnl + Number(pos.realized_profit);
+				}
+				if (pos.unrealized_profit && !isNaN(pos.unrealized_profit)) {
+					pnl = pnl + Number(pos.unrealized_profit);
+				}
+				Worker.logme(pos.symbol + " | " + pos.net_quantity + " | " + pnl);
+				mtm = mtm + pnl;
+			});
+			Worker.logme("--------------------------------------------");
+			Worker.logme("MTM: " + mtm);
+			Worker.logme("--------------------------------------------");
+		}).catch(function (error) {
+			//done(error);
+			Worker.logme("Error getting positions. "+JSON.stringify(error));
+		});
+	}
+
+	function exitAllPos() {
+		upstox.getPositions()
+		.then(function (response) {
+			var tPos = response.data;
+			//Worker.logme("Exiting all open positions..");
+			tPos.forEach(function (pos) {
+				if (pos.net_quantity != 0) {
+					Worker.logme(pos.symbol +" is open with " + pos.net_quantity +" qty. Exiting at market price.");
+					upstox.placeOrder({
+						"transaction_type": (pos.net_quantity > 0) ? "s" : "b",
+						"exchange": pos.exchange,
+						"symbol": pos.symbol,
+						"quantity": Math.abs(pos.net_quantity),
+						"order_type": "m",
+						"product": "I"
+					})
+					.then(function (response) {
+						Worker.logme(response.data.order_id + "|" + pos.symbol + "|" + response.data.status);
+					})
+					.catch(function (error) {
+						//done(error);
+						Worker.logme("Placing exit order error. "+JSON.stringify(error));
+					});
+				}
+			});
+		}).catch(function (error) {
+			//done(error);
+			Worker.logme("Error getting positions for exiting. "+JSON.stringify(error));
+		});
+	}
+
+	function diconnectSock() {
+		upstox.closeSocket();
+		console.log("\n\nDone.\n\n---------------------------------------------------------------");
+	}
+
 	initSetToken();
 	module.exports.initSetToken = initSetToken;
-	module.exports.strategyORB = function(){
-		selectScrips_HL(n50,3,"N50");
-		//selectScrips_HL(nfo,3, "ALL_FNO");
-	}
 	module.exports.getCurrentPos = getCurrentPos;
+	module.exports.cancellAllOrders = cancellAllOrders;
 	module.exports.exitAllPos = exitAllPos;
 	module.exports.diconnectSock = diconnectSock;
+	module.exports.strategyORB = function () {
+		if(appconts.stockpicks == "N50") selectScrips_HL(n50, 3, "N50");
+		if(appconts.stockpicks == "NFO") selectScrips_HL(nfo,3, "ALL_FNO");
+	}	
 }());
