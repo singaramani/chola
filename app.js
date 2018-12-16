740fac536d3ae1ba06d4250a7eec723044336364
singaramani <singaramani@gmail.com>
2018-12-16 11:20:13 +0400

    Update app.js

--- app.js
+++ app.js
@@ -13,55 +13,74 @@ const PORT = process.env.PORT || 8080;
 var appconts = Appconst.getAppConstants();
 
 /*Handling HTTP Req*/
-try{
+try {
 	http.createServer(function (request, response) {
 		var urlpath = request.url.split("?").shift();
-		if (urlpath == '/'+appconts.getCodeCommand) {
+		if (urlpath == '/' + appconts.getCodeCommand) {
 			response.writeHead(200, {'Content-Type': 'application/json'});
 			response.end(JSON.stringify({"command": "done"}));
-		}else if (urlpath == '/'+appconts.getCodeWrite) {
+		} else if (urlpath == '/' + appconts.getCodeWrite) {
 			getLoginToken();
 			response.writeHead(200, {'Content-Type': 'application/json'});
 			response.end(JSON.stringify({"command": "done"}));
-		}else if (urlpath == '/'+appconts.readRawCode) {
-			fs.readFile("/data/token.txt", "utf8", function(err, data){
-			//fs.readFile("D:\\token.txt", "utf8", function(err, data){
-				if(err) { return "file read error"; logme("file read error"); }
+		} else if (urlpath == '/' + appconts.readRawCode) {
+			fs.readFile("/data/token.txt", "utf8", function (err, data) {
+				//fs.readFile("D:\\token.txt", "utf8", function(err, data){
+				if (err) {
+					return "file read error";
+					logme("file read error");
+				}
 				response.writeHead(200, {'Content-Type': 'text/plain'});
 				response.end(data);
 				Worker.logme("Web read code invoked.")
 			});
-		}else if (urlpath == '/wakejob') {
+		} else if (urlpath == '/wakejob') {
 			wakeupJOB();
 			response.writeHead(200, {'Content-Type': 'application/json'});
 			response.end(JSON.stringify({"command": "done"}));
-		}else if (urlpath == '/tokenjob') {
+		} else if (urlpath == '/tokenjob') {
 			tokenJOB();
 			response.writeHead(200, {'Content-Type': 'application/json'});
 			response.end(JSON.stringify({"command": "done"}));
-		}else if (urlpath == '/tradejob') {
+		} else if (urlpath == '/tradejob') {
 			tradeJOB();
 			response.writeHead(200, {'Content-Type': 'application/json'});
 			response.end(JSON.stringify({"command": "done"}));
-		}else if (urlpath == '/rsall') {
+		} else if (urlpath == '/rsall') {
 			rescheduleAllJobs();
 			response.writeHead(200, {'Content-Type': 'application/json'});
 			response.end(JSON.stringify({"command": "done"}));
-		}else {
+		} else if (urlpath == '/posjob') {
+			positionJOB();
+			response.writeHead(200, {'Content-Type': 'application/json'});
+			response.end(JSON.stringify({"command": "done"}));
+		} else if (urlpath == '/canalljob') {
+			cancelAllOrdersJOB();
+			response.writeHead(200, {'Content-Type': 'application/json'});
+			response.end(JSON.stringify({"command": "done"}));
+		} else if (urlpath == '/exitposjob') {
+			exitPosJOB();
+			response.writeHead(200, {'Content-Type': 'application/json'});
+			response.end(JSON.stringify({"command": "done"}));
+		} else if (urlpath == '/disconsockjob') {
+			disconSockJOB();
+			response.writeHead(200, {'Content-Type': 'application/json'});
+			response.end(JSON.stringify({"command": "done"}));
+		} else {
 			response.writeHead(200, {'Content-Type': 'application/json'});
 			response.end(JSON.stringify({"command": "blank"}));
 		}
 	}).listen(PORT);
 	console.log('Running:' + PORT);
-}catch(e){
+} catch (e) {
 	console.log('httperr:' + e.message);
 }
 
-function getLoginToken(){
+function getLoginToken() {
 	Worker.fetchWriteToken(appconts.atCodeURL);
 }
 
-console.log("------------------------------------------------------------------------");  
+console.log("------------------------------------------------------------------------");
 
 // *    *    *    *    *    *
 // ┬    ┬    ┬    ┬    ┬    ┬
@@ -73,80 +92,117 @@ console.log("-------------------------------------------------------------------
 // │    └──────────────────── minute (0 - 59)
 // └───────────────────────── second (0 - 59, OPTIONAL)
 
-console.log("Today " + new Date().toLocaleString("en-US", {timeZone: "Asia/Kolkata"})+" IST");
+console.log("Today " + new Date().toLocaleString("en-US", {
+		timeZone: "Asia/Kolkata"
+	}) + " IST");
 
-function wakeupJOB(){
-	console.log("\n\n---------------------------------------------------------------"); 
-	Worker.logme("Today " + new Date().toLocaleString("en-US", {timeZone: "Asia/Kolkata"})+" IST");
-	Worker.logme("wakeupJOB Started"); 
+function wakeupJOB() {
+	console.log("\n\n---------------------------------------------------------------");
+	Worker.logme("Today " + new Date().toLocaleString("en-US", {
+			timeZone: "Asia/Kolkata"
+		}) + " IST");
+	Worker.logme("wakeupJOB Started");
 	Worker.wakeupServer(appconts.wakeupURL);
 }
 
-function tokenJOB(){
-	console.log("\n\n---------------------------------------------------------------"); 
-	Worker.logme("Today " + new Date().toLocaleString("en-US", {timeZone: "Asia/Kolkata"})+" IST");
-	Worker.logme("tokenJOB Started"); 
+function tokenJOB() {
+	console.log("\n\n---------------------------------------------------------------");
+	Worker.logme("Today " + new Date().toLocaleString("en-US", {
+			timeZone: "Asia/Kolkata"
+		}) + " IST");
+	Worker.logme("tokenJOB Started");
 	getLoginToken();
 }
 
-function tradeJOB(){
-	console.log("\n\n---------------------------------------------------------------"); 
-	Worker.logme("Today " + new Date().toLocaleString("en-US", {timeZone: "Asia/Kolkata"})+" IST");
-	Worker.logme("tradeJOB Started"); 
+function tradeJOB() {
+	console.log("\n\n---------------------------------------------------------------");
+	Worker.logme("Today " + new Date().toLocaleString("en-US", {
+			timeZone: "Asia/Kolkata"
+		}) + " IST");
+	Worker.logme("tradeJOB Started");
 	Trader.initSetToken();
 	Trader.strategyORB();
 }
 
-function positionJOB(){
-	Worker.logme("Getting positions.."); 
+function positionJOB() {
+	Worker.logme(" ");
+	Worker.logme("Getting positions..");
 	Trader.getCurrentPos();
 }
 
-function exitPosJOB(){
-	//Worker.logme("Getting positions.."); 
+function cancelAllOrdersJOB() {
+	Worker.logme(" ");
+	Worker.logme("Cancelling all open orders..");
+	Trader.cancellAllOrders();
+}
+
+function exitPosJOB() {
+	Worker.logme(" ");
+	Worker.logme("Getting positions..");
 	Trader.exitAllPos();
 }
 
-function disconSockJOB(){
-	//Worker.logme("Getting positions.."); 
+function disconSockJOB() {
+	Worker.logme(" ");
+	Worker.logme("Disconnecting socket..");
 	Trader.diconnectSock();
 }
 
-function scheduleTokenServerWakeup(){
-	console.log("Scheduling wakeupJOB..");  
-	scheduler.scheduleJob(appconts.wakeup_schedule, function (fireDate) {wakeupJOB();});
+function scheduleTokenServerWakeup() {
+	console.log("Scheduling wakeupJOB..");
+	scheduler.scheduleJob(appconts.wakeup_schedule, function (fireDate) {
+		wakeupJOB();
+	});
 }
 
-function scheduleTokenJOB(){
-	console.log("Scheduling tokenJOB..");  
-	scheduler.scheduleJob(appconts.token_schedule, function (fireDate) {tokenJOB();});
+function scheduleTokenJOB() {
+	console.log("Scheduling tokenJOB.. ");
+	scheduler.scheduleJob(appconts.token_schedule, function (fireDate) {
+		tokenJOB();
+	});
 }
 
-function scheduleTradeJOB(){
-	console.log("Scheduling tradeJOB..");
-	scheduler.scheduleJob(appconts.trade_schedule, function (fireDate) {tradeJOB();});
+function scheduleTradeJOB() {
+	console.log("Scheduling tradeJOB.. ");
+	scheduler.scheduleJob(appconts.trade_schedule, function (fireDate) {
+		tradeJOB();
+	});
 }
 
-function schedulePositionJOB(){
-	console.log("Scheduling positionJOB..");
-	scheduler.scheduleJob("30 4-10 * * *", function (fireDate) {positionJOB();});
+function schedulePositionJOB() {
+	console.log("Scheduling positionJOB.. ");
+	scheduler.scheduleJob(appconts.getpos_schedule, function (fireDate) {
+		positionJOB();
+	});
 }
 
-function scheduleExitPosJOB(){
-	console.log("Scheduling exitPosJOB..");
-	scheduler.scheduleJob("43 9 * * *", function (fireDate) {exitPosJOB();});
+function scheduleCancelAppOpenJOB() {
+	console.log("Scheduling cancelAllOpenJOB.. ");
+	scheduler.scheduleJob(appconts.cancelall_schedule, function (fireDate) {
+		cancelAllOrdersJOB();
+	});
 }
 
-function scheduleDisconnectSockJOB(){
-	console.log("Scheduling disconSockJOB..");
-	scheduler.scheduleJob("1 10 * * *", function (fireDate) {disconSockJOB();});
+function scheduleExitPosJOB() {
+	console.log("Scheduling exitPosJOB.. ");
+	scheduler.scheduleJob(appconts.exitpos_schedule, function (fireDate) {
+		exitPosJOB();
+	});
 }
 
-function rescheduleAllJobs(){
+function scheduleDisconnectSockJOB() {
+	console.log("Scheduling disconSockJOB.. ");
+	scheduler.scheduleJob(appconts.socket_schedule, function (fireDate) {
+		disconSockJOB();
+	});
+}
+
+function rescheduleAllJobs() {
 	scheduleTokenServerWakeup();
 	scheduleTokenJOB();
 	scheduleTradeJOB();
 	schedulePositionJOB();
+	scheduleCancelAppOpenJOB();
 	scheduleExitPosJOB();
 	scheduleDisconnectSockJOB();
 }rescheduleAllJobs();
