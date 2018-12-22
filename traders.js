b4ef3b2e0cc37d64558aa28ca7089a3d499c3b76
singaramani <singaramani@gmail.com>
2018-12-22 22:27:58 +0400

    Update traders.js

--- traders.js
+++ traders.js
@@ -357,7 +357,7 @@
 
 	function placeTargerOrder(pos) {
 		var txtTyp = (pos.transaction_type == "B" || pos.transaction_type == "b") ? "s" : "b";
-		var calc_tp = (txtTyp == "b") ? 1-(appconst.targetpcent/100) : 1+(appconst.targetpcent/100);
+		var calc_tp = (txtTyp == "b") ? 1-(appconst.targetpcent/100)-0.01 : 1+(appconst.targetpcent/100);
 		var tp = (Math.round((pos.average_price) * calc_tp * 20) / 20);
 		Worker.logme("Placing target order for " + txtTyp.toUpperCase() + " " + pos.symbol + " " + pos.traded_quantity + " @ " + tp + " ["+appconst.targetpcent+"%]");
 		upstox.placeOrder({
