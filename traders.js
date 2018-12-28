2c04b4dd99a39c3245a4e1615ed972162ef9b8a2
singaramani <singaramani@gmail.com>
2018-12-28 08:03:45 +0400

    Update traders.js

--- traders.js
+++ traders.js
@@ -14,7 +14,7 @@
     var qty = process.env.TQTY || 3;
     var trigger_offset = 0.10;
     var price_offset = 0.05;
-    var balperscrip = 2000;
+    var balperscrip = 130000;
     var restrictNewOrders = false;
     var placedOrders = [];
 
